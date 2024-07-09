bfda7966428c69d34aa40cf54bac1b0cf0616226
Fabiano Martins <fabianomartinsdev@gmail.com>
2024-07-09 20:15:14 -0300

    Home page started

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,50 @@
+import { MapPin, Calendar, ArrowRight } from "lucide-react";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div className="max-w-3xl w-full px-6 text-center space-y-10">
+        <p className="text-zinc-300 text-lg">
+          Convide seus amigos planeje sua próxima viagem!
+        </p>
+
+        <div className="h-16 bg-zinc-900 px-4 rounded flex items-center shadow-shape gap-3">
+          <div className="flex items-center gap-2 flex-1">
+            <MapPin className="size-5 text-zinc-400" />
+            <input
+              type="text"
+              placeholder="Para onde você vai?"
+              className="bg-transparent text-lg placeholder-zinc-400"
+            />
+          </div>
+          <div className="flex items-center gap-2 w-56">
+            <Calendar className="size-5 text-zinc-400" />
+            <input
+              type="text"
+              placeholder="Quando?"
+              className="bg-transparent text-lg placeholder-zinc-400 w-auto"
+            />
+          </div>
+          <div className="w-px h-6 bg-zinc-800"></div>
+          <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+            Continuar
+            <ArrowRight />
+          </button>
+        </div>
+
+        <p className="text-sm text-zinc-500">
+          Ao planejar sua viagem pelo plann.er você automaticamente concorda{" "}
+          <br /> com nossos{" "}
+          <a className="text-zinc-300 underline" href="#">
+            termos de uso
+          </a>{" "}
+          e{" "}
+          <a className="text-zinc-300 underline" href="#">
+            políticas de privacidade
+          </a>
+          .
+        </p>
+      </div>
+    </div>
+  );
 }
